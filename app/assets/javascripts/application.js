8fb54737af1552d56a13c1a7775db87f349dc059
Zoltan <zdebre@gmail.com>
2014-05-30 23:58:06 +1200

    Adding main template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,13 @@
 // require_self
 // require competition_app
 
-window.CompetitionApp = Ember.Application.create({});
+window.CompetitionApp = Ember.Application.create({
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_ACTIVE_GENERATION: true,
+  LOG_VIEW_LOOKUPS: true,
+  LOG_BINDINGS: true
+});
+
+Ember.run.backburner.DEBUG = true;
+Ember.ENV.RAISE_ON_DEPRECATION = true;
+Ember.LOG_STACKTRACE_ON_DEPRECATION = true;
